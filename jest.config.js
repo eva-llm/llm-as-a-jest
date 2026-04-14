defe7395fd3d23b74b0a10fa095b5272f64b01dc
Sergei Chipiga <chipiga86@gmail.com>
2026-04-15 00:51:57 +0300

    add tests coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,21 @@
 module.exports = {
   preset: 'ts-jest',
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  collectCoverageFrom: [
+    "src/**/*.{ts,js}",
+    "!src/**/*.d.ts",
+    "!src/types/**",
+    "!**/node_modules/**"
+  ],
+  coverageReporters: ["text", "lcov", "clover"],
+  coverageThreshold: {
+    global: {
+      branches: 63,
+      functions: 70,
+      lines: 84,
+      statements: 84,
+    },
+  },
 };
